2c602de4b69ab2ce718a6244ba00931ff3a837c5
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2024-10-02 20:25:01 +0300

    match illustrations with theme colors

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,6 +1,6 @@
-import seo from "../public/seo.png";
-import development from "../public/development.png";
-import social from "../public/social.png";
+import seo from "../public/seo.svg";
+import development from "../public/development.svg";
+import social from "../public/social.svg";
 import Image from "next/image";
 
 export default function Services() {
@@ -12,7 +12,7 @@ export default function Services() {
         </h3>
         <div className='flex flex-col gap-8 w-full'>
           <div className=' rounded-3xl bg-primary max-w-3xl flex flex-col justify-center items-center md:flex-row gap-4 px-4 py-8 border border-borderTwo shadow-lg mr-auto'>
-            <Image src={seo} alt='seo' height={150} />
+            <Image src={seo} alt='seo' height={250} />
             <div className='flex flex-col justify-start items-start'>
               <h6 className=' font-bold pb-2 text-secondary text-2xl'>
                 SEO & Local SEO
@@ -25,7 +25,7 @@ export default function Services() {
             </div>
           </div>
           <div className=' rounded-3xl bg-primary max-w-3xl flex flex-col justify-center items-center md:flex-row gap-4 px-4 py-8 border border-borderTwo shadow-lg ml-auto'>
-            <Image src={development} alt='web & mobile development' height={150} />
+            <Image src={development} alt='web & mobile development' height={250} />
             <div className='flex flex-col justify-start items-start'>
               <h6 className=' font-bold pb-2 text-secondary text-2xl'>
                 Web & Mobile development
@@ -38,7 +38,7 @@ export default function Services() {
             </div>
           </div>
           <div className=' rounded-3xl bg-primary max-w-3xl flex flex-col justify-center items-center md:flex-row gap-4 px-4 py-8 border border-borderTwo shadow-lg mr-auto'>
-            <Image src={social} alt='social media management' height={150} />
+            <Image src={social} alt='social media management' height={250} />
             <div className='flex flex-col justify-start items-start'>
               <h6 className=' font-bold pb-2 text-secondary text-2xl'>
                 Social media management
