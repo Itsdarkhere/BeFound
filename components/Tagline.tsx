fbbfa7506d8885cf40d6b7f9ca8724725cea741f
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2024-10-02 21:08:08 +0300

    stuff

--- components/Tagline.tsx
+++ components/Tagline.tsx
@@ -3,7 +3,7 @@ export default function Tagline() {
     <section className='w-full bg-accent px-4 relative flex flex-row justify-center items-center'>
       <div className='max-w-7xl mx-auto py-16 w-full flex flex-col md:flex-row justify-between items-start gap-2 md:items-center'>
         <h3 className=" text-secondary font-medium text-3xl">Your Business Deserves<br/>Professional Care</h3>
-        <span className=" text-primary text-base max-w-xl font-normal">
+        <span className=" text-textTwo text-base max-w-xl font-normal">
           In today&apos;s digital age, managing your online presence isn&apos;t just a
           side task—it&apos;s a full-time job. Your online presence deserves the
           attention of dedicated professionals.
