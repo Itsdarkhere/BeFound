a367c29288fc9e3f199a80113f3b430c508e2887
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2024-10-03 08:36:06 +0300

    stuff

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -8,7 +8,7 @@ export default function CTA() {
             <h4 className=" font-semibold text-secondary pb-2 text-4xl">Leave your email<br/>& we will get back to you</h4>
             <p className=" font-normal text-textTwo text-base pb-8">This alone commits you to nothing, we will email you and see if we can schedual a call to figure out if we can help!</p>
             <div className="flex flex-row shadow-md overflow-hidden rounded-xl">
-                <input className=" py-2 px-4 outline-none text-primary min-w-64" type="email" placeholder="Your email" />
+                <input className=" py-2 px-4 outline-none text-primary max-w-full w-64" type="email" placeholder="Your email" />
                 <button className=" px-6 py-3 flex justify-center items-center bg-primary text-secondary font-semibold">Send</button>
             </div>
         </div>
